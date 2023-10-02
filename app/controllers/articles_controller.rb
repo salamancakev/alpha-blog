268eeb07e6203648e11ccec7b2eefc6f93fb8116
Kevin Salamanca <kevinsalamanca26@gmail.com>
2023-10-02 12:21:46 -0400

    Added sessions, route guarding, and other features

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,13 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  
   def show
   end
 
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -17,7 +20,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
@@ -27,7 +30,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    
     if @article.update(article_params)
       flash[:notice] = "Article was updated successfully"
       redirect_to @article
@@ -52,5 +54,11 @@ class ArticlesController < ApplicationController
     params.require(:article).permit(:title, :description)
   end
 
+  def require_same_user
+    if current_user != @article.user || !current_user.admin?
+      flash[:alert] = "You can only edit or delete your own article"
+      redirect_to @article
+    end
+  end
 
 end
